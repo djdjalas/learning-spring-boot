2dd375ddec844eb5c9e96be7ad16d11500d09bb6
Nelson Djalo <iamdjalas@gmail.com>
2017-11-29 23:02:56 +0000

    Initial commit

---
+++ src/main/java/com/yourname/learningspringboot/LearningSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.yourname.learningspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringBootApplication.class, args);
+	}
+}
